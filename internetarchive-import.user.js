e7d30af2d0acfc6ea50e471957211e1bba336edb
Jens Bertram <code@jens-bertram.net>
2014-09-26 10:24:32 +0200

    updated link functions updated description add support for search result scanning refactoring

--- internetarchive-import.user.js
+++ internetarchive-import.user.js
@@ -1,10 +1,11 @@
 ﻿// ==UserScript==
 // @name        MusicBrainz: Archive.org importer
 // @namespace   http://www.jens-bertram.net/userscripts/import-internetarchive
-// @description Import audio files and collections into Musicbrainz.
+// @description Import audio files and collections into Musicbrainz. Also supports scanning bookmarks and search results for MusicBrainz relations.
 // @icon				http://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Internet_Archive_logo_and_wordmark.png/240px-Internet_Archive_logo_and_wordmark.png
 // @include     *://archive.org/details/*
 // @include			*://archive.org/bookmarks.php
+// @include			*//archive.org/search.php*
 // @require     https://code.jquery.com/jquery-2.1.1.min.js
 // @require     https://greasyfork.org/scripts/5140-musicbrainz-function-library/code/MusicBrainz%20function%20library.js
 // @version     0.3.0beta
@@ -26,6 +27,8 @@ mbz.archive_org_importer = {
 
 	/**
 		* Insert a link, if a release has MusicBrainz relations.
+		* @data key=mbid value=string array: relation types
+		* @target target jQuery element to append (optional) or this.bmLinkTarget set in scope
 		*/
 	insertMBLink: function(data, target) {
 		if (data) {
@@ -36,23 +39,52 @@ mbz.archive_org_importer = {
 			}
 			$.each(data, function(k, v) {
 				if (!k.startsWith('_')) { // skip internal data
-					var relLink = MBZ.Html.getLinkElement('release', k, "Linked as: " + v.toString());
-					target.after(relLink).after('&nbsp;');
-					var editLink = ', <a href="' + MBZ.Html.getLink('release', k, '/edit') + '" target="_blank" title="Edit release">edit</a>';
-					var artLinkTitle = '<b>set art</b>';
+					var relLink = MBZ.Html.getLinkElement({
+						type: 'release',
+						id: k,
+						title: "Linked as: " + v.toString(),
+						before: '&nbsp;'
+					});
+					target.after(relLink);
+					var editLink = MBZ.Html.getLinkElement({
+						type: 'release',
+						id: k,
+						more: 'edit',
+						text: 'edit',
+						title: 'Edit release',
+						before: ', ',
+						icon: false
+					});
+					var artLinkTitle = 'set';
 					$.ajax({
-						url: MBZ.CA.getLink('release', k, 'front')
+						url: MBZ.CA.getLink({
+							type: 'release',
+							id: k,
+							more: 'front'
+						})
 					}).success(function(){
-						artLinkTitle = 'edit art';
+						artLinkTitle = 'edit';
 					}).always(function() {
-						var artLink = ', <a href="' + MBZ.Html.getLink('release', k, '/cover-art') + '" target="_blank"  title="Set cover art">' + artLinkTitle + '</a>';
-						relLink.after('<sup> ' + v.length + editLink + artLink + '</sup>');
+						var artLink = MBZ.Html.getLinkElement({
+							type: 'release',
+							id: k,
+							more: 'cover-art',
+							text: artLinkTitle + ' art',
+							title: artLinkTitle + ' cover art for release',
+							before: ', ',
+							icon: false
+						});
+						relLink.after('<sup> ' + v.length + editLink.html() + artLink.html() + '</sup>');
 					});
 				}
 			});
 		}
 	},
 
+	/**
+		* Check file type for audio format.
+		* @formatStr file format name
+		*/
 	isAudioFile: function(formatStr) {
 		formatStr = formatStr.toLowerCase();
 		for (format in this.audioFormats) {
@@ -64,6 +96,118 @@ mbz.archive_org_importer = {
 	}
 };
 
+/**
+	* Functions to parse a list of links for MusicBrainz relations.
+	*/
+mbz.archive_org_importer.linkCheck = {
+	btn: MBZ.Html.getMbzButton('Check link relations', 'Check entries being linked from MusicBrainz.'),
+
+	/**
+		* Link scanner status values
+		*/
+	links: {
+		found: null,
+		checked: 0,
+		matched: 0
+	},
+
+	/**
+		* RexEx to strip off current base-url.
+		*/
+	re: new RegExp('^'+window.location.origin),
+
+	/**
+		* Scan status elements.
+		*/
+	status: {
+		base: $('<span>'),
+		current: $('<span>'),
+		matched: $('<span>')
+	},
+
+	/**
+		* Start scanner.
+		*	@params[links] jQuery object with target links
+		* @params[controlEl] jQuery element to append controls to
+		*/
+	scan: function(params) {
+		this.links.found = params.links;
+
+		if (this.links.found.length > 0 && params.controlEl) {
+			var self = this;
+			//var cEl = $('<div id="mbzControls">');
+
+			this.status.current.text(this.links.checked);
+			this.status.matched.text(this.links.matched);
+			this.status.base.append('&nbsp;Checked: ')
+				.append(this.status.current)
+				.append('&nbsp;Matches: ')
+				.append(this.status.matched)
+				.hide();
+
+			this.btn.click(function () {
+				self.btn.prop("disabled", true);
+				self.btn.text("Checking..");
+				self.status.base.show();
+				var urls = [];
+				$.each(self.links.found, function(idx, link) {
+					urls.push('http://archive.org'+$(link).attr('href'));
+				});
+				MBZ.Release.getUrlRelations({
+					urls: MBZ.Util.expandProtocols(urls),
+					cb: self.rel.attach,
+					cbInc: self.rel.inc,
+					cbDone: self.rel.done,
+					scope: self
+				});
+			});
+			//cEl.append(this.btn).append(this.status.base);
+			params.controlEl.append(this.btn).append(this.status.base);
+		}
+	},
+
+	/**
+		* Callback handlers for relation parsing.
+		*/
+	rel: {
+		/**
+			* Relation was found, data is attached.
+			*/
+		attach: function(data) {
+			if (!data._res) {
+				return;
+			}
+			var res = data._res.replace(this.re, '');
+			var self = this;
+			$.each(self.links.found, function(idx, link) {
+				var link = $(link);
+				if (link.attr('href') == res) {
+					self.status.matched.text(self.links.matched++);
+					mbz.archive_org_importer.insertMBLink(data, link);
+				}
+			});
+		},
+
+		/**
+			* All relations have been resolved.
+			*/
+		done: function() {
+			this.status.base.html('&nbsp;' + this.links.checked + ' links checked with ' + this.links.matched + ' matches.');
+			this.btn.text('Check done');
+		},
+
+		/**
+			* A relation was checked.
+			*/
+		inc: function() {
+			this.status.current.text(this.links.checked++);
+		},
+	}
+}
+
+/**
+	* Functions to import a single release.
+	*/
 mbz.archive_org_importer.release = {
 	btn: MBZ.Html.getMbzButton('Import', 'Import this release to MusicBrainz'),
 	mbLinkTarget: null,
@@ -97,7 +241,6 @@ mbz.archive_org_importer.release = {
 			self.parseJSON.tracks.call(self, trackData);
 			// *** parsed data from release JSON object
 			$.getJSON(urlJSON, function (data) {
-				console.log(data);
 				self.parseJSON.urls.call(self, data);
 				self.parseJSON.artists.call(self, data);
 				self.parseJSON.title.call(self, data);
@@ -204,105 +347,56 @@ mbz.archive_org_importer.release = {
 	release: new MBZ.Release()
 };
 
-mbz.archive_org_importer.bookmarks = {
-	btn: MBZ.Html.getMbzButton('Check link relations', 'Check entries being linked from MusicBrainz.'),
-
-	links: {
-		found: null,
-		checked: 0,
-		matched: 0
-	},
-
-	re: new RegExp('^'+window.location.origin),
-
-	status: {
-		base: $('<span>'),
-		current: $('<span>'),
-		matched: $('<span>')
-	},
-
-	init: function() {
-		this.links.found = $('.box>table>tbody a').filter(function(idx) {
-				return $(this).attr('href').startsWith('/details/');
-		});
-		if (this.links.found.length > 0) {
-			var self = this;
-			var cEl = $('<div id="mbzControls">');
-			//var btn = MBZ.Html.getMbzButton('Check link relations', 'Check entries being linked from MusicBrainz.');
-
-			this.status.current.text(this.links.checked);
-			this.status.matched.text(this.links.matched);
-			this.status.base.append('&nbsp;Checked: ')
-				.append(this.status.current)
-				.append('&nbsp;Matches: ')
-				.append(this.status.matched)
-				.hide();
-
-			this.btn.click(function () {
-				self.btn.prop("disabled", true);
-				self.btn.text("Checking..");
-				self.status.base.show();
-				var urls = [];
-				$.each(self.links.found, function(idx, link) {
-					urls.push('http://archive.org'+$(link).attr('href'));
-				});
-				MBZ.Release.getUrlRelations({
-					urls: MBZ.Util.expandProtocols(urls),
-					cb: self.rel.attach,
-					cbInc: self.rel.inc,
-					cbDone: self.rel.done,
-					scope: self
-				});
-			});
-			cEl.append(this.btn).append(this.status.base);
-			$('.box>h1').after(cEl);
-		}
-	},
-
-	/**
-		* Callback handlers for relation parsing.
-		*/
-	rel: {
-		/**
-			* Relation was found, data is attached.
-			*/
-		attach: function(data) {
-			if (!data._res) {
-				return;
-			}
-			var res = data._res.replace(this.re, '');
-			var self = this;
-			$.each(self.links.found, function(idx, link) {
-				var link = $(link);
-				if (link.attr('href') == res) {
-					self.status.matched.text(self.links.matched++);
-					mbz.archive_org_importer.insertMBLink(data, link);
-				}
-			});
-		},
-
-		/**
-			* All relations have been resolved.
-			*/
-		done: function() {
-			this.status.base.html('&nbsp;' + this.links.checked + ' links checked with ' + this.links.matched + ' matches.');
-			this.btn.text('Check done');
-		},
-
-		/**
-			* A relation was checked.
-			*/
-		inc: function() {
-			this.status.current.text(this.links.checked++);
-		},
-	}
-}
-
 mbz.archive_org_importer.init = function() {
-	if ($('body').hasClass('Audio')) { // basic data type check
+	var pageType = window.location.pathname.split('/');
+	if (pageType.length >= 2) {
+		pageType = pageType[1].toLowerCase()
+	} else {
+		return;
+	}
+
+	if (pageType == 'details' && $('body').hasClass('Audio')) {
+		// import a release
 		mbz.archive_org_importer.release.init();
-	} else if ($('body').hasClass('bookmarks')) {
-		mbz.archive_org_importer.bookmarks.init();
+	} else if (pageType == 'bookmarks.php') {
+		// check all bookmarks for MusicBrainz relations
+		var links = $('.box>table>tbody a').filter(function(idx) {
+			// no way to check type for audio here
+			return $(this).attr('href').startsWith('/details/');
+		});
+		var control = $('<div id="mbzControls">');
+		$('.box>h1').after(control);
+		if (links.length > 0) {
+			mbz.archive_org_importer.linkCheck.scan({
+				links: links,
+				controlEl: control
+			});
+		}
+	} else if (pageType == 'search.php') {
+		var links = [];
+		// check audio links for MusicBrainz relations
+		var audioItems = $('.numberCell>img[alt="[audio]"]').filter(function(idx) {
+			// get the first linked audio item..
+			var el = $(this).parent().next().children('a')[0];
+			if (el) {
+				el = $(el);
+				if (el.attr('href').startsWith('/details/')) {
+					// ..and extract it's url
+					links.push(el);
+				}
+			}
+		});
+		var control = $('<div>');
+		var col = $('<td colspan="2">');
+		col.append(control);
+		var row = $('<tr>').append(col);
+		$('.resultsTable').prepend(row);
+		if (links.length > 0) {
+			mbz.archive_org_importer.linkCheck.scan({
+				links: links,
+				controlEl: control
+			});
+		}
 	}
 };
 
